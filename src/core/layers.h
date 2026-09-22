5e7cc94d10b840c16bdcf94971e324d976e40174
Dmitry Sokolov <ha@haqr.eu>
2026-09-22 11:17:05 +0200

    re-indentation

--- src/core/layers.h
+++ src/core/layers.h
@@ -1,35 +1,29 @@
 #pragma once
 #include "core.h"
 
-// -------------------------------------------------------------------------------
-//                                    RenderLayer + LayerManager
-// -------------------------------------------------------------------------------
-// => 
-// 
-
-
-struct RenderLayer{
+struct RenderLayer {
     virtual ~RenderLayer() = default;
-	virtual void render()		=0;
-	virtual void generate_gui(std::string name) =0;
-	virtual bool resync_with_data()=0;
-	virtual void render_primitive_id()				{Log::add("To be implemented");}
-	virtual void render_constant_color(int layerid)	{Log::add("To be implemented"); }
-	bool visible;
+    virtual void render() = 0;
+    virtual void generate_gui(std::string name) = 0;
+    virtual bool resync_with_data()=0;
+    virtual void render_primitive_id()              { Log::add("To be implemented"); }
+    virtual void render_constant_color(int layerid) { Log::add("To be implemented"); }
+    bool visible;
 };
 
 struct LayerManager: public Registry<RenderLayer> {
-	void render(){
-		for (auto& [name,obj] : *this)
+    void render() {
+        for (auto& [name,obj] : *this)
             obj->render();
-	}
-	void sync(){ // TODO: separate the cleanup logic from the update
-		FOR(i,size()) if (!operator[](i).resync_with_data()){
-			std::swap(items[i],items.back());
-			pop_back();
-		};
-	}
-	void produce_picking_image(int* data,int w,int h){ Log::add("To be implemented"); }
+    }
+    void sync() { // TODO: separate the cleanup logic from the update
+        FOR(i,size())
+            if (!operator[](i).resync_with_data()) {
+                std::swap(items[i],items.back());
+                pop_back();
+            }
+    }
+    void produce_picking_image(int* data,int w,int h) { Log::add("To be implemented"); }
 };
 
 
